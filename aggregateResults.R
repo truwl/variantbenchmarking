9a52663460aa9b7a73ba3bcca455b32bef199a0c
Jeremy Leipzig <leipzig@gmail.com>
2021-06-15 15:01:41 -0600

    region

--- aggregateResults.R
+++ aggregateResults.R
@@ -1,12 +1,12 @@
 #!/usr/bin/env Rscript
 library(dplyr)
 library(tidyr)
-##USAGE: Rscript aggregateResults.R job_id workflow_instance_identifier workflow_identifier [files]
+##USAGE: Rscript aggregateResults.R job_id workflow_instance_identifier workflow_identifier wes cds outputfile
 
 
 #args = commandArgs(trailingOnly=TRUE)
 args = commandArgs(TRUE)
-if (length(args)<5)
+if (length(args)<6)
   stop("Expecting job inst work [files]")
 
 job_id<-args[1]
@@ -17,16 +17,19 @@ wes<-args[4]
 cds<-args[5]
 
 outputfile <- args[6]
-read.csv(wes,header=TRUE) %>% dplyr::mutate(WorkflowId=workflow_id,
-                                            WorkflowInstanceID=workflow_inst,
-                                            JobRunID=job_id,
-                                            Type='WholeExome') -> temp1
 
 read.csv(wes,header=TRUE) %>% dplyr::mutate(WorkflowId=workflow_id,
                                             WorkflowInstanceID=workflow_inst,
                                             JobRunID=job_id,
-                                            Type='CodingSequence') -> temp2
+                                            Region='WholeExome') -> temp1
+
+read.csv(wes,header=TRUE) %>% dplyr::mutate(WorkflowId=workflow_id,
+                                            WorkflowInstanceID=workflow_inst,
+                                            JobRunID=job_id,
+                                            Region='CodingSequence') -> temp2
 
 #rbind(temp1,temp2) %>% reshape2::melt(.id.vars=c('WorkflowInstanceID','WorkflowId','JobRunID','Sample','Type,','Filter')) %>% write.table(row.names=FALSE,sep="\t",file="",quote=FALSE)
 
-rbind(temp1,temp2) %>% gather(key = "variable", value="value", -WorkflowInstanceID, -WorkflowId, -JobRunID, -Type, -Filter) %>% write.table(row.names=FALSE,sep="\t",file=outputfile,quote=FALSE)
+rbind(temp1,temp2) %>%
+  gather(key = "variable", value="value", -WorkflowInstanceID, -WorkflowId, -JobRunID, -Type, -Filter, -Region) %>%
+  write.table(row.names=FALSE,sep="\t",file=outputfile,quote=FALSE)
