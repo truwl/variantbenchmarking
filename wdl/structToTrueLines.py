5cd16887ff51ba772e5d85563c44dcc5f84836bc
Jeremy Leipzig <leipzig@gmail.com>
2021-09-17 10:31:17 -0600

    mydict

--- wdl/structToTrueLines.py
+++ wdl/structToTrueLines.py
@@ -6,11 +6,13 @@ import json
 s=sys.argv[1]
 quotedKeys = True
 
+mydict = {}
+
 #if the keys aren't quoted make them
 if quotedKeys:
     mydict = json.loads(s)
 else:
-    mydict=json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
+    mydict = json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
 
 filtered = [k for k, v in mydict.items() if v]
 
