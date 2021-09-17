c8608a0bbcc3aeaa8c3f729b5745c6adfa37c461
Jeremy Leipzig <leipzig@gmail.com>
2021-09-17 10:26:00 -0600

    no q

--- wdl/structToTrueLines.py
+++ wdl/structToTrueLines.py
@@ -4,11 +4,11 @@ import sys
 import json
 
 s=sys.argv[1]
-mydict=json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
+
+#if the keys aren't quoted
+#mydict=json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
 
 filtered = [k for k, v in mydict.items() if v]
 
 for l in filtered:
     print(l)
-
-print("smeg")
