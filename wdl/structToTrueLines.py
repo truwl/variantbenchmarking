e4e70189f0d458d0752e598818e53caf6050ead8
Jeremy Leipzig <leipzig@gmail.com>
2021-09-17 10:29:53 -0600

    quoted keys

--- wdl/structToTrueLines.py
+++ wdl/structToTrueLines.py
@@ -4,9 +4,13 @@ import sys
 import json
 
 s=sys.argv[1]
+quotedKeys = True
 
-#if the keys aren't quoted
-#mydict=json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
+#if the keys aren't quoted make them
+if quotedKeys:
+    mydict = json.loads(s)
+else:
+    mydict=json.loads(s.replace('{','{"').replace(':','":').replace(', ',', "'))
 
 filtered = [k for k, v in mydict.items() if v]
 
